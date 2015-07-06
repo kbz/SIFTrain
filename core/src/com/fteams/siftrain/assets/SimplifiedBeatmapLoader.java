6f5c55b079c97a75f11e4c0f7a4a98b5bfa7780f
Andrew "Akari" Alexeyew <akari@dbc.1gb.ua>
2015-07-06 17:21:31 +0300

    Actually fixed the regexp

--- core/src/com/fteams/siftrain/assets/SimplifiedBeatmapLoader.java
+++ core/src/com/fteams/siftrain/assets/SimplifiedBeatmapLoader.java
@@ -38,7 +38,7 @@ public class SimplifiedBeatmapLoader extends AsynchronousAssetLoader<BeatmapDesc
             beatmap.setFileName(fileName);
         }
         finally {
-            beatmap.setResourceName(handle.nameWithoutExtension().replaceAll("_(easy|norma|hard|expert)$", ""));
+            beatmap.setResourceName(handle.nameWithoutExtension().replaceAll("_(easy|normal|hard|expert)$", ""));
         }
     }
 
