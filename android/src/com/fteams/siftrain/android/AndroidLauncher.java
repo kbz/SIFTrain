96a50f9a46ff548a93e2a0b3df9e4c56eab22076
Clement Ng <clementng23@hotmail.com>
2016-01-08 03:45:50 +1100

    Android 6.x permission fix * Added permission check (and request) on startup * updated Gradle scripts to api 23 * ContextCompat and ActivityCompact support libs used for backwards compatibility

--- android/src/com/fteams/siftrain/android/AndroidLauncher.java
+++ android/src/com/fteams/siftrain/android/AndroidLauncher.java
@@ -1,8 +1,11 @@
 package com.fteams.siftrain.android;
 
+import android.Manifest;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -13,6 +16,16 @@ public class AndroidLauncher extends AndroidApplication {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+        if (ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
+
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         try {
             PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
